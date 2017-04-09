b44f0262e190c180a7a60d5d0b35bfc175293c39
EduardsKEA <edua0193@stud.kea.dk>
2017-04-09 19:06:33 +0200

    created getters setters for Player, class diagram added

--- src/domain/Player.java
+++ src/domain/Player.java
@@ -7,14 +7,50 @@ public class Player {
 
     private String playerName;
     private int matchesPlayed;
-    private String dob;
-    private int goalsScored;
+    private String dob; // DD.MM.YYYY, change to LocalDate
 
-    public Player(String playerName) {
+
+    public Player(String playerName, String dob) {
         this.playerName = playerName;
-        this.matchesPlayed = 0;
-        this.goalsScored = 0;
+        this.dob = dob;
     }
 
+    public void setMatchesPlayed(int matchesPlayed) {
+        if(matchesPlayed >= 0) {
+            this.matchesPlayed = matchesPlayed;
+        }else
+        {
+            throw new IllegalArgumentException("Matches played can't be negative!");
+            //this.matchesPlayed = 0;
+        }
+    }
+    public void updateMatchesPlayed(int matchesPlayed) {
+        if(matchesPlayed >= 0) {// FIXME: 4/9/2017 consider checking sum greater than 0
+            this.matchesPlayed += matchesPlayed;
+        }else
+        {
+            throw new IllegalArgumentException("Matches played can't be negative!");
+            //this.matchesPlayed = 0;
+        }
+    }
 
+    public void setPlayerName(String playerName) {// FIXME: 4/9/2017 name should not be empty yea
+        this.playerName = playerName;
+    }
+
+    public void setDateOfBirth(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public String getDob() {
+        return dob;
+    }
 }
