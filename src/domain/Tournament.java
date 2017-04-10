7c45de14e0004031ee75203c64fa9fc86de92571
GerliP <gerl0016@stud.kea.dk>
2017-04-10 15:08:23 +0300

    Added lists to store teams, players and matches and created method to create matches

--- src/domain/Tournament.java
+++ src/domain/Tournament.java
@@ -1,7 +1,41 @@
 package domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by ArcticMonkey on 4/9/2017.
  */
 public class Tournament {
+
+    List<Match> matchList = new LinkedList<>();
+    List<Team> teamList = new LinkedList<>();
+    List<Player> playerList = new LinkedList<>();
+
+    public void registerPlayer (Player p){
+
+    playerList.add(p);
+    }
+
+    public void registerTeam (Team t){
+
+        teamList.add(t);
+    }
+
+    public void createMatches(){
+        for(int i = 0; i< teamList.size()-1; i++){
+            Team team1 = teamList.get(i);
+            for(int j = i+1; j <teamList.size(); j++){
+                Team team2 = teamList.get(j);
+                String matchName = team1.getTeamName() + " vs " + team2.getTeamName();
+                matchList.add(new Match(matchName, team1, team2));
+            }
+
+        }
+
+
+
+    }
+
 }
+
